b7c5227123ef0a077d1fcbd2b421e563efc9a884
R4tmax <kadlec.m.90@gmail.com>
2024-10-23 19:24:30 +0200

    Rearrange the core CSS files, clean up Vue starter boilerplate

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+
+    <section>
+      <h1>Hello there!</h1>
+      <p>
+        Martin Kadlec je jméno mé, a je mi ctí vás přívítat v mém kousku internetu,
+        který jsem začal budovat, abych měl místo, kde podrobovat své myšlenky soudu a testu času a veřejnosti.
+      </p>
+
+      <p>
+        Ve zkratce: Jsem softwarový inženýr, rádoby filozof, vášnivý čtenář a v obecné rovině sběratel různorodých a podivných aktivit a zájmů.
+        V životě jsem měl doposud tu čest věnovat se automatizačním projektům českého IT, transhumanismu, atletice, výuce angličtiny a mnohému dalšímu.
+        Pokud se chcete dozvědět více o mne, stačí si nastudovat následující <RouterLink to="/about">podstránku</RouterLink>!
+      </p>
+    </section>
+
   </main>
 </template>
