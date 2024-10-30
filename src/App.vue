320fd70678c54bad5587813bd53e2f04bf0a4a98
R4tmax <kadlec.m.90@gmail.com>
2024-10-30 15:45:14 +0100

    Clean up home page, add font awesome icon support

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,23 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-
   <header>
-    <h1 class="header-name"><RouterLink active-class="header-name-rl-override" exact-active-class="header-name-rl-override" to="/">Martin Kadlec</RouterLink></h1>
-      <div class="nav-wrapper">
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-          <RouterLink to="/blog">Blog</RouterLink>
-          <!-- <RouterLink to="/cv">CV</RouterLink> -->
-        </nav>
-      </div>
+    <h1 class="header-name">
+      <RouterLink
+        active-class="header-name-rl-override"
+        exact-active-class="header-name-rl-override"
+        to="/"
+        >Martin Kadlec</RouterLink
+      >
+    </h1>
+    <div class="nav-wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/blog">Blog</RouterLink>
+        <!-- <RouterLink to="/cv">CV</RouterLink> -->
+      </nav>
+    </div>
   </header>
 
   <main>
@@ -21,13 +27,26 @@ import { RouterLink, RouterView } from 'vue-router'
   </main>
 
   <footer>
-    <div class="footer-item"> &copy; Martin Kadlec - 2024 </div>
-    <div class="footer-item"> Hosted via Github Pages</div>
-    <div class="footer-item"> 
-      <div class="footer-subitem"><a href="mailto:kadm09@vse.cz" target="_blank" class="cv-quick-link">Napište mi!</a></div>
-      <div class="footer-subitem"><a class="cv-card" href="https://github.com/R4tmax" target="_blank"> <i class="fa-brands fa-github" aria-hidden="true"></i>
-        GitHub</a></div>
-      <div class="footer-subitem">Spolupráce?</div>
+    <div class="footer-item">&copy; Martin Kadlec - 2024</div>
+    <div class="footer-item">
+      <div class="footer-subitem">
+        Hosted via Github Pages
+        <i class="fa-brands fa-github" aria-hidden="true"></i>
+        - <a href="https://github.com/R4tmax/R4tmax.github.io" target="_blank">Repository</a>
+      </div>
+    </div>
+    <div class="footer-item">
+      <div class="footer-subitem">
+        <a href="mailto:kadm09@vse.cz" target="_blank">
+          <i class="fa-regular fa-envelope"></i> Napište mi!</a
+        >
+      </div>
+      <div class="footer-subitem">
+        <a href="https://github.com/R4tmax" target="_blank">
+          <i class="fa-brands fa-github" aria-hidden="true"></i> GitHub</a
+        >
+      </div>
+      <!-- <div class="footer-subitem">Spolupráce?</div> -->
     </div>
   </footer>
 </template>
