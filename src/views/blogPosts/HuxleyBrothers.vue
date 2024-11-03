1708d34796f85f71cc301d5fdd3b637ce2b631f5
R4tmax <kadlec.m.90@gmail.com>
2024-11-03 19:38:49 +0100

    Add info card support, initiate the HB article content

--- src/views/blogPosts/HuxleyBrothers.vue
+++ src/views/blogPosts/HuxleyBrothers.vue
@@ -1,3 +1,45 @@
 <template>
-    
+    <div class="info-card">Následující článek vychází z textů, které jsem vypracoval v rámci bakalářského studia v předmětech Filozofie pro informatiky a Fenomén myšlení, které nikdy nebyly publikované mimo půdu VŠE a budou přiloženy v rámci referencí na konci článku. V tomto článku lze pracovat s premisou, že zdroje použité v těcho pracech jsou referencemi sekundárními pro celý tento článek s různou mírou relevance, a to do doby než dokončím ruční validaci a ozdrojování jednotlivých pasáží. </div>
+    <h1>Příběh bratří Huxleyů, aneb co je to Transhumanismus, a proč by mě to mělo zajímat?</h1>
+    <section>
+        <p>Nehledě na vaše zázemí, přesvědčení, původ a náturu, se asi všichni dokážeme shodnout na tom, že poslední roky spolu přinášejí mnoho bezprecedentních překvapení a změn.
+            Veřejnou debatou válcují témata jako je umělá inteligence, význam a pozice veřejného zdravotnictví ve společnosti, informační gramotnost a mnoho, mnoho dalšího.
+            Když člověk vezme tyto témata pod mikroskop, mohl by si všimnout toho, že na jejich pozadí se opakují jisté vzorce.
+        </p>
+        <p>Na poli informatiky a informačních technologií dochází ke komprehensivní transformaci sektoru směrem ke cloud-driven technologiím, velké korporáty začínají investovat
+            do jaderné energetiky, aby dokázali pokrýt energetické nároky nových technologií a tzv. "informační" věk kulminuje ve jménu velkých datových modelů a vším s tím spojeným.
+            Ve zdravotnictví se odmazává hranice mezi umělým a přirozeným za pomocí vývojů na poli materiálů, strojírenství, genového inžernýrství a využití datových technologií pro prediktivní a preskriptivní řešení prolémů jako jsou autoimunitní choroby a rakovina. 
+        </p>
+
+        <p> Na následujících řádcích vám budu vyprávět příběh bratří Huxleyů, budu vám vyprávět příběh o tom, jak tyto věci nejsou náhlým a nečekaným vývojem na poli technologií, nýbrž
+            realizací dlouholeté vize, která lomcovala (a stále lomcuje) úvahami filozofů, inženýrů, specialistů na etiku, biologů a lidí nejrůznějších povolání. Budu vám vyprávět příběh o tom,
+            jakým směrem vane vítr současné technologické inovace a odkud se tento vítr vůbec vzal. 
+        </p>
+    </section>
+
+    <!-- <div class="direct-quote-highlight">
+        Patient: "Time changes everything." Gregory House: "That’s what people say. It’s not true. Doing things changes things. Not doing things leaves things exactly as they were." ~ House, M.D. 
+    </div> -->
+
+    <h2>Kde se vzal, tu se vzal</h2>
+    <section>
+        <p>Pojďme to vzít všechno popořádku, a zodpovězme si tu nejzákladnější otázku - co to ten Transhumanismus vůbec je?</p>
+        <p>Všichni asi tak nějak intuitivně tuší, co to je Humanismus - myšlenkový směr pocházející z Renesance, kdy od dob antiky v Evropě poprvé došlo k odklonu od náboženského dogmatu a společnost se začala otáčet směrem k vědě a sekulární vládě. Humanisté v různé formě předsvědčení došli k závěru, že náboženské orgnaizace a jejich vliv na společnost jsou překážkou lidského pokroku a společnost tedy musí projít změnou a získat nové povědomí o světě - povědomí nezatížené křesťasnkou myšlenkou.</p>
+        <p>Když si člověk do Googlu hodí dotaz na význam prefixu <em>trans-</em>, není těžké vydedukovat, oč nám tu běží. Transhumanismus lze volně přepsat jako post-humanismus či "beyond" humanismus. Zatímco humanisté se zabývali limitacemi člověka způsobené společností, transhumanisté se zabývají limitacemi člověka způsobené člověkem. Podle transhumanistů by každý jedinec měl mít právo a možnost změnit či vylepšit své tělo (a mysl) skrze genetické a kybernetické modifikace, těžit z technologického pokroku lidstva a žít takový život, který si přeje žít, alespoň do takové míry, kde neomezuje ostatní. Podle transhuministů ultimátní odpovědí na všechny lidské problémy je technologický pokrok a aplikace tohoto pokroku nejen na náš svět, ale na nás samé. V extrémních případech by takový člověk (zde zcela bez nadsázky) měl přestat být <em>pouhým homo sapiens</em> ale něčím novým a lepším, doslova a do písmene <em>Beyond human, transhuman,superhuman</em>. Terminologie se může lišit, finální záměr nikoliv.</p>
+
+        <p>Myšlenku "nadčlověka" mají všichni tak nějak nevyhnutelně spojenou s krutou realitou světových válek, což není náhoda, pro všechny praktické a filozofické potřeby ji poprvé vyslovil Friedrich Nietzsche. Není náhoda, že přelom 19. a 20. století byl zrozením Nihilismu, Existencionalismu a Absurdismu v podstatě ve stejném časovém rozpětí (což je diskuze pro jiný den a článek) a není náhoda že všechny tyto filozife se snažili zodpovědět fundamentální otázku toho, co to vůbec znamená být člověkem. Dvacáté století je totiž mimo jiné zrod Ideologií v celé své šíři a kráse jak je známe dnes, a jedna z těchto ideologií byl i ideál druhoválečného Německa, který stavěl (mimo jiné) na pervertovaných a špatně interpretovaných ideálech Nihilismu a Nietzscheho prací.</p>
+
+        <p>Je to přesně v této době, v letech předcházející zrodu Třetí Říše. Kdy na scénu vstupuje první bratr z našeho příběhu, Aldous Huxley.</p>
+    </section>
+
+    <h2>Brave New World</h2>
+    <section>
+        <p>
+            Aldous Huxley byl vnuk Thomase Henry Huxleyho, což byl jeden z předních propnentů Darwinovy Evoluční teorie, a pocházel z rodiny zasvěcené biologii a akademickému životu. Osud však Aldouse zavál trochu jiným směrem a vyrostl z něj spisovatel a filozof. Aldous se narodil v roce 1894, a v útlém věku prodělal oční zánět a kvůli zdravotním problémům byl zproštěn vojenské služby.
+        </p>
+
+        <p>Po válce Aldous začal publikovat sociální satiru, a s přibývajícím věkem si v kontextu vrcholu tzv. "Machine Age" (kterou nahradil Atomový Věk) a techno-optimismu panující tehdejší společností vybudoval tučnou dávku skepticismu vůči tomuto pokroku, a rozhodl se spojit své zázemí z biologie, skepticismus a kritiku tohoto (as far as I can tell dle jeho osobního uvážení naivního) optimismu do jednoho díla: Brave New World, které vyšlo v roce 1932.</p>
+
+        <p>BNW se odehrává ve futuristické verzi Londýna, kde společnost používá genové terapie, mimoděložní reprodukci a pharmakoterapii pro regulaci lidských emocí. Zjednodušeně řečeno Aldous napsal dystopický román (tehdejší) perspektivou Ad Absurdum technologického pokroku, a varoval před sterilitou, dehumanizací a jinými nástrahami společnosti posedlé vědou a efektivitou. Bez toho, aniž bych se pouštěl do spoilerů nebo sáhodlouhých rešerší - zatímco Orwell se bál diktatury, A. Huxley se bál toho, že žádná diktatura nebude třeba a lidé za správnou cenu odevzdají svoji lidskost a svobodu sami - nechám na uvážření každého čtenáře, který obraz je bližší realitě.</p>
+    </section>
 </template>
